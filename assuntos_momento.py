9cb4ff9a2228957363e3eb27f5933744d5dc560f
Marcos Felipe <116011701+MarcoFelipe999@users.noreply.github.com>
2023-02-20 02:59:32 -0300

    Update assuntos_momento.py

--- assuntos_momento.py
+++ assuntos_momento.py
@@ -9,11 +9,11 @@ def extracao_twitter():
     driver.get(url=link)
     print('Tendências de Brasil')
     sleep(5)
-    trend_elements = driver.find_elements(By.XPATH, "//div[@data-testid='trend']//span")
+    elements = driver.find_elements(By.XPATH, "//div[@data-testid='trend']//span")
     trend_list = [ ]
-    for trend_element in trend_elements:
-        trend_name = trend_element.text
-        trend_list.append(trend_name)
+    for trend_element in elements:
+        name = trend_element.text
+        trend_list.append(name)
 
     for trend_att in trend_list:
         print(trend_att)

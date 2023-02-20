6950973b75e4b25cd73e9eddce6da09378bccc9d
MarcoFelipe999 <mf931089@gmail.com>
2023-02-20 02:58:15 -0300

    twitter

---
+++ assuntos_momento.py
@@ -0,0 +1,27 @@
+
+from selenium.webdriver.common.by import By
+from time import sleep
+from selenium import webdriver
+
+def extracao_twitter():
+    driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
+    link = 'https://twitter.com/explore/tabs/trending'
+    driver.get(url=link)
+    print('Tendências de Brasil')
+    sleep(5)
+    trend_elements = driver.find_elements(By.XPATH, "//div[@data-testid='trend']//span")
+    trend_list = [ ]
+    for trend_element in trend_elements:
+        trend_name = trend_element.text
+        trend_list.append(trend_name)
+
+    for trend_att in trend_list:
+        print(trend_att)
+
+
+
+extracao_twitter()
+
+
+
+
